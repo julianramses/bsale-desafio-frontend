6f120b0977b1e81827335d1d7f20de7992e4519e
Julian Escobar <julianramses@MBP-de-Julian.lan>
2022-06-16 12:45:51 -0400

    frontend

---
+++ public/javascripts/bsale.js
@@ -0,0 +1,20 @@
+(async function(){
+    const pelement = document.getElementById('productos')
+    const req = await fetch('http://bsaleapiv5.herokuapp.com/productos/productos/')
+    const productos = await req.json() 
+    const search = await req.json()
+    productos.productos.forEach(producto => {
+        let template = 
+        `<img class="card-img-top img-thumbnail"  src="${producto.url_image}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${producto.name}</h5>
+                <p class="card-text">Precio: $${producto.price}</p>
+                DESCUENTO ${producto.discount}%<span class=""badge badge-danger"></span>
+            </div>`
+       let element = document.createElement('div')
+       element.className = "card"
+       element.style = "width: 18rem;"
+       element.innerHTML = template
+       pelement.append(element)
+    });
+})()
